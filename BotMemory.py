0913fa34a92c5737e5ae1aab5864c61a670c3fe6
amir <amir.harati@gmail.com>
2018-04-02 19:11:39 -0700

    add more functionality to the memory module

--- BotMemory.py
+++ BotMemory.py
@@ -5,6 +5,7 @@
 
 from neo4jrestclient.client import GraphDatabase
 from neo4jrestclient import client
+from random import randrange
 
 
 class BotMemory:
@@ -44,6 +45,9 @@ class BotMemory:
             n.relationships.create("located", l)
 
     def clear_memory(self):
+        """
+           forget everything.
+        """
         q = "MATCH (n:user_name) DETACH DELETE n"
         results = self.db.query(q, returns=(client.Node, str, client.Node))
         q = "MATCH (n:restaurant_name) DETACH DELETE n"
@@ -52,13 +56,6 @@ class BotMemory:
         results = self.db.query(q, returns=(client.Node, str, client.Node))
         q = "MATCH (n:location) DETACH DELETE n"
         results = self.db.query(q, returns=(client.Node, str, client.Node))
-
-    def _memorize(self, entity1, entity2, rel):
-        e1 = self._add_node(entity1['name'], entity1['type'])
-        e2 = self._add_node(entity2['name'], entity2['type'])
-        # create a relationship
-        # BUG: can create multiple links
-        e1.relationships.create(rel, e2)
 
     def _add_node(self, node_name, node_type):
         """
@@ -77,8 +74,41 @@ class BotMemory:
             r = results[0][0]
         return r
 
+    def get_user_location(self, u):
+        loc = None
+        q = 'MATCH p=(u:user_name {name:"' + u + '"})-[:located]->(ul:location) RETURN ul'
+        results = self.db.query(q, data_contents=True)
+        if len(results) > 0:
+            loc = results.rows[0][0]["name"]
+        return loc
 
+    def find_restaurant(self, u):
+        """
+           find a good choice!
+           Should handle when there is no result or
+           when user location is not specified.
+        """
+        r_name = None
+        r_location = None
+        r_cuisine = None
+        u_name = u
 
-    #def reterive(self, ...):
-    # need to be generic. For example in this case we want to incorprate
-    # location and cusine for a user but generally not sure
+        q1 = 'MATCH p=(rl:location)<-[:located]-(res:restaurant_name)-[r:is_a]->(c:cuisine)<-[:likes]-(u:user_name {name:"' + u + '"})-[:located]->(ul:location) where ul=rl RETURN res, c, u, rl  LIMIT 25'
+
+        results = self.db.query(q1, data_contents=True)
+        if (len(results) > 0):
+            random_index = randrange(0, len(results))
+            r_name = results.rows[random_index][0]["name"]
+            r_location = results.rows[random_index][3]["name"]
+            r_cuisine = results.rows[random_index][1]["name"]
+            return (u_name, r_name, r_location, r_cuisine)
+
+        q2 = 'MATCH p=(u:user_name {name:"' + u + '"})-[:located]->(ul:location)<-[:located]-(res:restaurant_name)  RETURN res, u, ul  LIMIT 25'
+        results = self.db.query(q2, data_contents=True)
+        if (len(results) > 0):
+            random_index = randrange(0, len(results))
+            r_name = results.rows[random_index][0]["name"]
+            r_location = results.rows[random_index][2]["name"]
+
+        return (u_name, r_name, r_location, r_cuisine)
+
